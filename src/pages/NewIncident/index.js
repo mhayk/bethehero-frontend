61cf2794f69024d0f0bd017bcce647de0262cdab
Mhayk Whandson <hi@mhayk.com>
2020-03-28 19:36:00 +0000

    The style of the page "New Incidents" has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+
 export default function NewIncidente() {
-  return <h1>New Incident</h1>;
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Add a new incident</h1>
+          <p>
+            Describe the incident in detail to find a hero to solve it.
+          </p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Back to home
+          </Link>
+        </section>
+        <form>
+          <input placeholder="NGO Title" />
+          <textarea placeholder="Description" />
+          <input placeholder="Value in Pounds" />
+
+
+          <button className="button" type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
 }
