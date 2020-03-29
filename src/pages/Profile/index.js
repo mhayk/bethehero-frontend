0686d260e15ce02d5d01e4f096fd5192330aa88e
Mhayk Whandson <hi@mhayk.com>
2020-03-29 14:18:37 +0100

    The value has been formatted with Int.NumberFormat.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId,
+      },
+    }).then((response) => {
+      setIncidents(response.data);
+    });
+  }, [ongId]);
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Welcome, APAD</span>
+        <span>
+          Welcome,
+          {' '}
+          {ongName}
+        </span>
 
         <Link className="button" to="/incidents/new">Add new incident</Link>
         <button type="button">
@@ -21,62 +42,31 @@ export default function Profile() {
       <h1>Incidents added</h1>
 
       <ul>
-        <li>
-          <strong>CASE:</strong>
-          <p>Test case</p>
+        {
+          incidents.map((incident) => (
+            <li key={incident.id}>
+              <strong>CASE:</strong>
+              <p>{incident.title}</p>
 
-          <strong>DESCRIPTION:</strong>
-          <p>Description test</p>
+              <strong>DESCRIPTION:</strong>
+              <p>{incident.description}</p>
 
-          <strong>VALUE:</strong>
-          <p>£ 120.00</p>
+              <strong>VALUE:</strong>
+              <p>
+                {
+                Intl.NumberFormat(
+                  'en',
+                  { style: 'currency', currency: 'GBP' },
+                ).format(incident.value)
+                }
+              </p>
 
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASE:</strong>
-          <p>Test case</p>
-
-          <strong>DESCRIPTION:</strong>
-          <p>Description test</p>
-
-          <strong>VALUE:</strong>
-          <p>£ 120.00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASE:</strong>
-          <p>Test case</p>
-
-          <strong>DESCRIPTION:</strong>
-          <p>Description test</p>
-
-          <strong>VALUE:</strong>
-          <p>£ 120.00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASE:</strong>
-          <p>Test case</p>
-
-          <strong>DESCRIPTION</strong>
-          <p>Description test</p>
-
-          <strong>VALUE:</strong>
-          <p>£ 120.00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+              <button type="button">
+                <FiTrash2 size={20} color="#a8a8b3" />
+              </button>
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
